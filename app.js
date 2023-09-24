56385a9182b3053193cdf04fbaf1cc965dbb96db
konghiu <conghieucoconcuto@gmail.com>
2023-09-24 22:30:24 +0700

    edit app.js 2.1

--- app.js
+++ app.js
@@ -32,13 +32,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "src/public")));
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        optionsSuccessStatus: 200,
-        credentials: true,
-    })
-);
+const allowCors = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
+app.use(cors());
 app.use(cookieParser());
 app.use(function (req, res, next) {
     res.header("Content-Type", "application/json;charset=UTF-8");
