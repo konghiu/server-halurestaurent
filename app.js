a8fefc23c8509066f8f5d49fd5f2e6c7e7af2622
konghiu <conghieucoconcuto@gmail.com>
2023-09-24 22:22:57 +0700

    edit app.js 2.0

--- app.js
+++ app.js
@@ -34,8 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "src/public")));
 app.use(
     cors({
-        origin: "https://localhost:3000",
-        // origin: "https://konghiu.github.io",
+        origin: "http://localhost:3000",
         optionsSuccessStatus: 200,
         credentials: true,
     })
